4e4f1f1ce4d3dbc135f03d4954d20ebf3bcf4c7e
Sergey Maskalik <mercury2269@gmail.com>
2012-11-25 21:56:35 -0800

    initial commit

---
+++ iTunesManager/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace iTunesManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLocationOld = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtLocationNew = new System.Windows.Forms.TextBox();
+            this.checkBoxRemove = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.checkBoxRelocate = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 25);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(756, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(347, 12);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(142, 23);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Stop";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Find Dead Tracks";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(756, 492);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Track Title";
+            this.columnHeader1.Width = 201;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Artist";
+            this.columnHeader2.Width = 129;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "File Location";
+            this.columnHeader3.Width = 129;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Bit Rate";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Find Duplicates";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.checkBoxRemove);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.buttonCancel);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(756, 97);
+            this.panel1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxRelocate);
+            this.groupBox1.Controls.Add(this.txtLocationOld);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.txtLocationNew);
+            this.groupBox1.Location = new System.Drawing.Point(15, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(729, 46);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Relocate Tracks";
+            // 
+            // txtLocationOld
+            // 
+            this.txtLocationOld.Location = new System.Drawing.Point(6, 19);
+            this.txtLocationOld.Name = "txtLocationOld";
+            this.txtLocationOld.Size = new System.Drawing.Size(142, 20);
+            this.txtLocationOld.TabIndex = 8;
+            this.txtLocationOld.Text = "\\\\OldLocation";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(326, 16);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 24);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Change Location";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtLocationNew
+            // 
+            this.txtLocationNew.Location = new System.Drawing.Point(167, 19);
+            this.txtLocationNew.Name = "txtLocationNew";
+            this.txtLocationNew.Size = new System.Drawing.Size(142, 20);
+            this.txtLocationNew.TabIndex = 9;
+            this.txtLocationNew.Text = "M:\\NewLocation";
+            // 
+            // checkBoxRemove
+            // 
+            this.checkBoxRemove.AutoSize = true;
+            this.checkBoxRemove.Location = new System.Drawing.Point(515, 17);
+            this.checkBoxRemove.Name = "checkBoxRemove";
+            this.checkBoxRemove.Size = new System.Drawing.Size(145, 19);
+            this.checkBoxRemove.TabIndex = 6;
+            this.checkBoxRemove.Text = "Remove When Found";
+            this.checkBoxRemove.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 97);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(756, 492);
+            this.panel2.TabIndex = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.progressBar1);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 589);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(756, 48);
+            this.panel3.TabIndex = 7;
+            // 
+            // checkBoxRelocate
+            // 
+            this.checkBoxRelocate.AutoSize = true;
+            this.checkBoxRelocate.Location = new System.Drawing.Point(500, 19);
+            this.checkBoxRelocate.Name = "checkBoxRelocate";
+            this.checkBoxRelocate.Size = new System.Drawing.Size(148, 19);
+            this.checkBoxRelocate.TabIndex = 11;
+            this.checkBoxRelocate.Text = "Relocate When Found";
+            this.checkBoxRelocate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 637);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Name = "Form1";
+            this.Text = "iTunes COM Sample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.CheckBox checkBoxRemove;
+        private System.Windows.Forms.TextBox txtLocationNew;
+        private System.Windows.Forms.TextBox txtLocationOld;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBoxRelocate;
+    }
+}
+
---
+++ iTunesManager/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections;
+using iTunesLib;
+
+namespace iTunesManager
+{
+    public partial class Form1 : Form
+    {
+        private volatile bool _shouldStop;
+        private Thread worker;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RemoveDuplicates()
+        {
+            //create a reference to iTunes
+            iTunesAppClass iTunes = new iTunesAppClass();
+
+            //get a reference to the collection of all tracks
+            IITTrackCollection tracks = iTunes.LibraryPlaylist.Tracks;
+
+            int trackCount = tracks.Count;
+            int numberChecked = 0;
+            int numberDuplicateFound = 0;
+            Dictionary<string, IITTrack> trackCollection = new Dictionary<string, IITTrack>();
+            ArrayList tracksToRemove = new ArrayList();
+
+            //setup the progress control
+            this.SetupProgress(trackCount);
+
+            for (int i = trackCount; i > 0; i--)
+            {
+                if (tracks[i].Kind == ITTrackKind.ITTrackKindFile)
+                {
+                    if (!this._shouldStop)
+                    {
+                        numberChecked++;
+                        this.IncrementProgress();
+                        this.UpdateLabel("Checking track # " + numberChecked.ToString() + " - " + tracks[i].Name);
+                        string trackKey = tracks[i].Name + tracks[i].Artist + tracks[i].Album;
+
+                        if (!trackCollection.ContainsKey(trackKey))
+                        {
+                            trackCollection.Add(trackKey, tracks[i]);
+                        }
+                        else
+                        {
+                            if (trackCollection[trackKey].Album != tracks[i].Album || trackCollection[trackKey].Artist != tracks[i].Artist)
+                            {
+                                trackCollection.Add(trackKey, tracks[i]);
+                            }
+                            else if (trackCollection[trackKey].BitRate > tracks[i].BitRate)
+                            {
+                                IITFileOrCDTrack fileTrack = (IITFileOrCDTrack)tracks[i];
+                                numberDuplicateFound++;
+                                tracksToRemove.Add(tracks[i]);
+                            }
+                            else
+                            {
+                                IITFileOrCDTrack fileTrack = (IITFileOrCDTrack)tracks[i];
+                                trackCollection[trackKey] = fileTrack;
+                                numberDuplicateFound++;
+                                tracksToRemove.Add(tracks[i]);
+                            }                            
+                        }
+                    }
+                }                                
+            }
+
+            this.SetupProgress(tracksToRemove.Count);
+
+            for (int i = 0; i < tracksToRemove.Count; i++)
+            {
+                IITFileOrCDTrack track = (IITFileOrCDTrack)tracksToRemove[i];
+                this.UpdateLabel("Removing " + track.Name);
+                this.IncrementProgress();
+                this.AddTrackToList((IITFileOrCDTrack)tracksToRemove[i]);
+
+                if (this.checkBoxRemove.Checked)
+                {
+                    track.Delete();
+                }
+            }
+
+            this.UpdateLabel("Checked " + numberChecked.ToString() + " tracks and " + numberDuplicateFound.ToString() + " duplicate tracks found.");
+            this.SetupProgress(1);
+        }
+
+        private void FindDeadTracks()
+        {
+            //create a reference to iTunes
+            iTunesAppClass iTunes = new iTunesAppClass();
+
+            //get a reference to the collection of all tracks
+            IITTrackCollection tracks = iTunes.LibraryPlaylist.Tracks;
+
+            int trackCount = tracks.Count;
+            int numberChecked = 0;
+            int numberDeadFound = 0;
+
+            //setup the progress control
+            this.SetupProgress(trackCount);
+
+            for (int i = trackCount; i > 0; i--)
+            {
+                if (!this._shouldStop)
+                {
+                    IITTrack track = tracks[i];
+                    numberChecked++;
+                    this.IncrementProgress();
+                    this.UpdateLabel("Checking track # " + numberChecked.ToString() + " - " + track.Name);
+                    
+                    if (track.Kind == ITTrackKind.ITTrackKindFile)
+                    {
+                        IITFileOrCDTrack fileTrack = (IITFileOrCDTrack)track;                        
+
+                        //if the file doesn't exist, we'll delete it from iTunes
+                        if (fileTrack.Location == String.Empty)
+                        {
+                            numberDeadFound++;
+                            this.AddTrackToList(fileTrack);
+
+                            if (this.checkBoxRemove.Checked)
+                            {
+                                fileTrack.Delete();
+                            }
+                        }
+                        else if (!System.IO.File.Exists(fileTrack.Location))
+                        {
+                            numberDeadFound++;
+                            this.AddTrackToList(fileTrack);
+
+                            if (this.checkBoxRemove.Checked)
+                            {
+                                fileTrack.Delete();
+                            }
+                        }
+                    }
+                }
+            }
+
+            this.UpdateLabel("Checked " + numberChecked.ToString() + " tracks and " + numberDeadFound.ToString() + " dead tracks found.");
+            this.SetupProgress(1);
+        }
+
+        private void ChangeTrackLocation()
+        {
+            //create a reference to iTunes
+            var iTunes = new iTunesAppClass();
+
+            //get a reference to the collection of all tracks
+            IITTrackCollection tracks = iTunes.LibraryPlaylist.Tracks;
+
+            int trackCount = tracks.Count;
+            int numberChecked = 0;
+            int numberRelocateFound = 0;
+
+            //setup the progress control
+            SetupProgress(trackCount);
+
+            for (int i = trackCount; i > 0; i--)
+            {
+                if (!_shouldStop)
+                {
+                    IITTrack track = tracks[i];
+                    numberChecked++;
+                    IncrementProgress();
+                    UpdateLabel("Relocating track # " + numberChecked.ToString(CultureInfo.InvariantCulture) + " - " + track.Name);
+
+                    if (track.Kind == ITTrackKind.ITTrackKindFile)
+                    {
+                        var fileTrack = (IITFileOrCDTrack)track;
+
+                        //if the file matches our old location
+                        if (fileTrack.Location != null && fileTrack.Location.StartsWith(txtLocationOld.Text))
+                        {
+                            //check if file exists in the new location
+                            var newLocation = fileTrack.Location.Replace(txtLocationOld.Text, txtLocationNew.Text);
+                            if (System.IO.File.Exists(newLocation))
+                            {
+                                numberRelocateFound++;
+                                AddTrackToList(fileTrack);
+
+                                if (checkBoxRelocate.Checked)
+                                {
+                                    fileTrack.Location = newLocation;
+                                }
+                            }
+                            
+
+                        }
+                    }
+                }
+            }
+
+            this.UpdateLabel("Checked " + numberChecked.ToString() + " tracks and " + numberRelocateFound.ToString() + " relocate tracks found.");
+            this.SetupProgress(1);
+        }
+
+        #region Message Handlers
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this._shouldStop = true;
+            this.buttonCancel.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.label1.Text = "";
+            this.buttonCancel.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this._shouldStop = false;
+            this.buttonCancel.Enabled = true;
+            this.listView1.Items.Clear();
+
+            this.worker = new Thread(this.FindDeadTracks);
+            this.worker.Start();
+        }
+        #endregion
+
+        #region Delegate Callbacks
+        //delagates for thread-safe access to UI components
+        delegate void SetupProgressCallback(int max);
+        delegate void IncrementProgressCallback();
+        delegate void UpdateLabelCallback(string text);
+        delegate void CompleteOperationCallback(string message);
+        delegate void AddTrackToListCallback(IITFileOrCDTrack fileTrack);
+
+        private void IncrementProgress()
+        {
+            if (this.progressBar1.InvokeRequired)
+            {
+                IncrementProgressCallback cb = new IncrementProgressCallback(IncrementProgress);
+                this.Invoke(cb, new object[] { });
+            }
+            else
+            {
+                this.progressBar1.PerformStep();
+            }
+        }
+
+        private void UpdateLabel(string text)
+        {
+            if (this.label1.InvokeRequired)
+            {
+                UpdateLabelCallback cb = new UpdateLabelCallback(UpdateLabel);
+                this.Invoke(cb, new object[] { text });
+            }
+            else
+            {
+                this.label1.Text = text;
+            }
+        }
+
+        private void CompleteOperation(string message)
+        {
+            if (this.label1.InvokeRequired)
+            {
+                CompleteOperationCallback cb = new CompleteOperationCallback(CompleteOperation);
+                this.Invoke(cb, new object[] { message });
+            }
+            else
+            {
+                this.label1.Text = message;
+            }
+        }
+
+        private void AddTrackToList(IITFileOrCDTrack fileTrack)
+        {
+            if (this.listView1.InvokeRequired)
+            {
+                AddTrackToListCallback cb = new AddTrackToListCallback(AddTrackToList);
+                this.Invoke(cb, new object[] { fileTrack });
+            }
+            else
+            {
+                this.listView1.Items.Add(new ListViewItem(new string[] { fileTrack.Name, fileTrack.Artist, fileTrack.Location, fileTrack.BitRate.ToString() }));
+            }
+        }
+
+        private void SetupProgress(int max)
+        {
+            if (this.progressBar1.InvokeRequired)
+            {
+                SetupProgressCallback cb = new SetupProgressCallback(SetupProgress);
+                this.Invoke(cb, new object[] { max });
+            }
+            else
+            {
+                this.progressBar1.Maximum = max;
+                this.progressBar1.Minimum = 1;
+                this.progressBar1.Step = 1;
+                this.progressBar1.Value = 1;
+            }
+        }
+        #endregion
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this._shouldStop = false;
+            this.buttonCancel.Enabled = true;
+            this.listView1.Items.Clear();
+
+            this.worker = new Thread(this.RemoveDuplicates);
+            this.worker.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this._shouldStop = false;
+            this.buttonCancel.Enabled = true;
+            this.listView1.Items.Clear();
+
+            this.worker = new Thread(this.ChangeTrackLocation);
+            this.worker.Start();
+        }
+    }
+}
